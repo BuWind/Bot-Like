b8fd570e626a731b5fdd6f1444d5b51c96841d4f
BuWind <54036609+BuWind@users.noreply.github.com>
2019-08-12 22:26:52 +0200

    Update menu.js

--- .gitignore/menu.js
+++ .gitignore/menu.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const superagent = require('superagent');
 
 client.login(process.env.TOKEN);
 
@@ -14,7 +15,14 @@ client.on("message", (message) => {
     if(message.content === prefix + "help"){
         message.channel.send("Veux-tu de l'aide?")
     }
+    
+     if(cmd === `${prefix}cat`) {
+        let msg = await message.channel.send("Generating...")
 
+        let {body} = await superagent
+        .get('http://aws.random.cat/meow')
+        console.log(body.file)
+    }
 
 
 
